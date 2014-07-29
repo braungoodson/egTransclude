2f218d36fe564e3eceb1fa01bdfe14081d531da0
Braun Goodson <braungoodson@gmail.com>
2014-07-28 23:21:43 -0400

    ok

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ eg.directives =  eg.directives || {};
 eg.Router = Router;
 
 eg.directives.SimpleTransclusion = SimpleTransclusion;
+eg.directives.Navigator = Navigator;
 
 function Router ($routeProvider) {
 	$routeProvider.when('/home',{templateUrl:'views/home.html'});
@@ -26,8 +27,23 @@ function SimpleTransclusion () {
 	};
 }
 
+function Navigator ($location) {
+	return {
+		restrict: 'A',
+		link: function (scope,element) {
+			scope.$on('$locationChangeSuccess',function(){
+				var children = element.children()
+				children.find('a').parent().removeClass('active');
+				children.find('a[href="#'+$location.path()+'"]').parent().addClass('active');
+			});
+		}
+	};
+}
+
 var app = angular.module('app',['ngRoute']);
 
 app.config(['$routeProvider',eg.Router]);
 
 app.directive('egSimpleTransclusion',[eg.directives.SimpleTransclusion]);
+
+app.directive('egNavigator',['$location',eg.directives.Navigator]);
