3df5d1c7660391f04eb512fd7d43187f1b4e3c74
Braun Goodson <braungoodson@gmail.com>
2014-08-10 12:24:56 -0400

    fixed clock

--- index.js
+++ index.js
@@ -64,10 +64,37 @@ angular
 		$interval(tick,1000);
 		function tick () {
 			var date = new Date();
-			var hours = date.getHours() % 12;
+			var hours = date.getHours();
 			var minutes = date.getMinutes();
 			var seconds = date.getSeconds();
-			var meridiem = hours >= 12 ? 'AM' : 'PM';
+			var meridiem;
+			switch (hours) {
+				case 23: hours = "11"; meridiem = "PM"; break;
+				case 22: hours = "10"; meridiem = "PM"; break;
+				case 21: hours = "09"; meridiem = "PM"; break;
+				case 20: hours = "08"; meridiem = "PM"; break;
+				case 19: hours = "07"; meridiem = "PM"; break;
+				case 18: hours = "06"; meridiem = "PM"; break;
+				case 17: hours = "05"; meridiem = "PM"; break;
+				case 16: hours = "04"; meridiem = "PM"; break;
+				case 15: hours = "03"; meridiem = "PM"; break;
+				case 14: hours = "02"; meridiem = "PM"; break;
+				case 13: hours = "01"; meridiem = "PM"; break;
+				case 12: hours = "12"; meridiem = "PM"; break;
+				case 11: hours = "11"; meridiem = "AM"; break;
+				case 10: hours = "10"; meridiem = "AM"; break;
+				case 9: hours = "09"; meridiem = "AM"; break;
+				case 8: hours = "08"; meridiem = "AM"; break;
+				case 7: hours = "07"; meridiem = "AM"; break;
+				case 6: hours = "06"; meridiem = "AM"; break;
+				case 5: hours = "05"; meridiem = "AM"; break;
+				case 4: hours = "04"; meridiem = "AM"; break;
+				case 3: hours = "03"; meridiem = "AM"; break;
+				case 2: hours = "02"; meridiem = "AM"; break;
+				case 1: hours = "01"; meridiem = "AM"; break;
+				case 0: hours = "12"; meridiem = "AM"; break;
+				default: hours = undefined;
+			}
 			minutes = minutes < 10 ? '0' + minutes : minutes;
 			seconds = seconds < 10 ? '0' + seconds : seconds;
 			date = hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
